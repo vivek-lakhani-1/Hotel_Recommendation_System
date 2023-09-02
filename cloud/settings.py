cbe433483d980f264614fa9b2b03eaa9b7eadfe7
Vivek S. Lakhani <88651044+vivek-lakhani-1@users.noreply.github.com>
2023-09-02 13:39:19 +0530

    Update settings.py

--- cloud/settings.py
+++ cloud/settings.py
@@ -128,7 +128,7 @@ STATICFILES_DIRS = [
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS = [BASE_DIR / "static"] # new
-STATIC_ROOT = BASE_DIR / "staticfiles" # new
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
